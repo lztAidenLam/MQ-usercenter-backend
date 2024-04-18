605ab7485a45ddc83b87dd5a44b195874236ea82
aidenlam <lztaidenlam@gmail.com>
2024-04-18 14:54:19 +0800

    新增 Knife4j 接口文档并配置

---
+++ src/main/java/com/lam/usercenter/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.lam.usercenter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+/**
+ * @author AidenLam
+ * @date 2024/4/18
+ */
+@Configuration
+@EnableSwagger2WebMvc
+@Profile({"dev", "test"})  // 版本控制访问
+public class SwaggerConfig {
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                // 这里一定要标注你控制器的位置
+                .apis(RequestHandlerSelectors.basePackage("com.lam.usercenter.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * api 信息
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("煤球用户中心")
+                .description("煤球用户中心文档")
+                .termsOfServiceUrl("https://github.com/lztAidenLam")
+                .contact(new Contact("AidenLam","https://github.com/lztAidenLam","lztaidenlam@gmail.com"))
+                .version("1.0")
+                .build();
+    }
+}
